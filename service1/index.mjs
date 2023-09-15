34f31abdf5265ae3e97b55c2f43a652eaa30a5a7
Joonas Pelttari <joonas.pelttari98@gmail.com>
2023-09-15 03:02:40 +0300

    refactor + logging

--- service1/index.mjs
+++ service1/index.mjs
@@ -1,64 +1,31 @@
-import axios from 'axios';
-import fs from 'fs'
-import path from 'path';
 import dns from 'dns';
+import { 
+  writeToLogFile, 
+  createOrClearLogFile, 
+  sleep, 
+  send }
+from './utils/utils.mjs';
 
+// configs
 const service2name = 'service2joonaspelttari';
 const service2PORT = 8000;
 let service2Address;
 
 // get address of service 2
 dns.lookup(service2name, (error, address) => {
-  if (error) {
-    console.error('Error getting service2 address:', error);
-  } else {
-    service2Address = address;
-  }
+  if (!error) { service2Address = address; }
+  startLogger();
 });
 
-// file writing setups
-const filename = new URL(import.meta.url).pathname;
-const dirname = path.dirname(filename);
-const filepath = path.join(dirname, '/logs/testi.txt');
-console.log("PATHIII:", filepath);
-
-const writeToLogFile = (text) => {
-  const textWithNewline = text+'\n'
-  try {
-    fs.appendFileSync(filepath, textWithNewline);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-const sleep = (timeMS) => {
-  return new Promise(resolve => setTimeout(resolve, timeMS));
-};
-
-const send = async (index) => {
-  const URL = 'http://'+service2name+':'+service2PORT;
-  try {
-    const res = await axios.get(URL, {
-      data: {
-        moi: "Joonas!!!!",
-        index,
-      },
-      timeout: 5000,
-    });
-  } catch (error) {
-    console.log(error)
+const startLogger = async () => {
+  createOrClearLogFile();
+  let i = 1;
+  while (i<8){
+    const date = new Date();
+    let text = `${i} ${date.toISOString()} ${service2Address}:${service2PORT}`;
+    writeToLogFile(text);
+    await sleep(2000);
+    await send(i, service2name, service2PORT);
+    i++;
   }
 };
-
-console.log("service 1 running");
-let i = 1;
-while (i<5){
-  const date = new Date();
-  let text = i+' '+date.toISOString();
-  writeToLogFile(text);
-  await sleep(2000);
-  await send(i);
-
-  i++;
-}
-console.log("service 1 closing");
---
+++ service1/utils/utils.mjs
@@ -0,0 +1,57 @@
+import fs from 'fs'
+import path from 'path';
+import axios from 'axios';
+
+// file writing setups
+const filename = new URL(import.meta.url).pathname;
+const dirname = path.dirname(filename);
+const filepath = path.join(dirname, '../logs/service1.log');
+
+/**
+ * Write text to log file and add newline
+ * @param {*} text to write
+ */
+export const writeToLogFile = (text) => {
+  const textWithNewline = text+'\n'
+  try {
+    fs.appendFileSync(filepath, textWithNewline);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * Creates the log file or if it exists, clears/wipes it.
+ */
+export const createOrClearLogFile = () => {
+  try {
+    fs.writeFileSync(filepath, '');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * @param {*} timeMS time to sleep as milliseconds
+ */
+export const sleep = (timeMS) => {
+  return new Promise(resolve => setTimeout(resolve, timeMS));
+};
+
+/**
+ * todo
+ */
+export const send = async (index, service2name, service2PORT) => {
+  const URL = 'http://'+service2name+':'+service2PORT;
+  try {
+    const res = await axios.get(URL, {
+      data: {
+        moi: "Joonas!!!!",
+        index,
+      },
+      timeout: 5000,
+    });
+  } catch (error) {
+    console.log(error)
+  }
+};
