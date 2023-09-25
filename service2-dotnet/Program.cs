61adebc9b1f379a93caa3ee241da10ec3644b9e5
Joonas Pelttari <joonas.pelttari98@gmail.com>
2023-09-26 02:32:47 +0300

    service2 ready

--- service2-dotnet/Program.cs
+++ service2-dotnet/Program.cs
@@ -10,21 +10,23 @@ using System.Text.Json;
 class Program {
   private static readonly string _serviceName = "service2joonaspelttari";
   private static readonly string _port = "8000";
+  private static readonly string _url = "http://"+_serviceName+":"+_port;
 
   public static void Main() {
+    CreateEmptyLogFile();
+    Thread.Sleep(2000);
     var host = new WebHostBuilder()
       .UseKestrel()
-      .UseUrls("http://"+_serviceName+":"+_port)
+      .UseUrls(_url)
       .Configure(app => {
         app.Run(async (context) => {
           string text = await GetValueByKeyFromRequestBody(context, "text");
-          //Console.WriteLine(text);
-          if (text.Equals("STOP")) { 
-            CloseApp(); 
+          if (text.Equals("STOP")) {
+            ExitApp(app);
           } else {
+            text = CreateLogText(context, text);  
             WriteToLogFile(text);
           }
-          await context.Response.WriteAsync("hello Joonas");
         });
       })
       .Build();
@@ -32,13 +34,13 @@ class Program {
     host.Run();
   }
 
-  private async static Task<string> GetValueByKeyFromRequestBody
+  private static async Task<string> GetValueByKeyFromRequestBody
     (HttpContext context, string key) {
-    var body = await new StreamReader(context.Request.Body)
-      .ReadToEndAsync();
-    var parsedBody = JsonSerializer.Deserialize<JsonElement>(body);
     string text = "";
     try {
+      var body = await new StreamReader(context.Request.Body)
+        .ReadToEndAsync();
+      var parsedBody = JsonSerializer.Deserialize<JsonElement>(body);  
       text = parsedBody.GetProperty(key).ToString();
     } catch (Exception e) {
       Console.WriteLine($"error: {e.Message}");
@@ -47,17 +49,37 @@ class Program {
   }
 
   private static void WriteToLogFile(string text) {
-
+    try {
+      string exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
+      string exeDir = Path.GetDirectoryName(exePath);
+      using StreamWriter writer = new(exeDir + "/logs/service2.log", true);
+      writer.WriteLine(text);
+      writer.Close();
+    } catch (Exception e) {  
+      Console.WriteLine("error creating log file");
+    }
   }
 
-  private static void CreateOrClearLogFile() {
-
+  private static void CreateEmptyLogFile() {
+    try {
+      string exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
+      string exeDir = Path.GetDirectoryName(exePath);
+      using StreamWriter writer = new(exeDir + "/logs/service2.log");
+      writer.Close();
+    } catch (Exception e) {  
+      Console.WriteLine("error creating log file");
+    }
   }
 
-  private static void CloseApp() {
-    Environment.Exit(5);
+  private static string CreateLogText(HttpContext context, string text) {
+    string address = context.Connection.RemoteIpAddress.ToString();
+    string port = context.Connection.RemotePort.ToString();
+    return text + " " + address + ":" + port;
+  }
+
+  private static void ExitApp(IApplicationBuilder app) {
+    app.ApplicationServices.GetRequiredService
+      <IHostApplicationLifetime>().StopApplication();
   }
 
 }
-
-
