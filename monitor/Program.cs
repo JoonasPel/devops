f123fd4f408de1d9bf5a1d4bb86e36b9963f5b9e
Joonas Pelttari <joonas.pelttari98@gmail.com>
2023-10-23 21:11:22 +0300

    monitor formatting

--- monitor/Program.cs
+++ monitor/Program.cs
@@ -8,39 +8,45 @@ using RabbitMQ.Client.Events;
 using System.Text;
 
 
-public class Program {
+public class Program
+{
   private static readonly string?
   _port = Environment.GetEnvironmentVariable("monitorPort")
   , _containerName = Environment.GetEnvironmentVariable("monitorContainerName")
-  , _url = "http://"+_containerName+":"+_port;
+  , _url = "http://" + _containerName + ":" + _port;
   private static IModel? rabbitChannel;
   private static List<string> _logTexts = new List<string>();
 
-  public static void Main() {
+  public static void Main()
+  {
     Thread.Sleep(20000); // todo wait-for-it.sh
     rabbitChannel = ConnectToRabbit();
     var consumer = CreateRabbitConsumer();
     StartRabbitConsuming(consumer);
 
     var host = CreateWebHost();
-    AppDomain.CurrentDomain.ProcessExit += async (sender, e) => {
+    AppDomain.CurrentDomain.ProcessExit += async (sender, e) =>
+    {
       await host.StopAsync();
     };
     host.Run();
   }
 
-  private static IModel ConnectToRabbit() {
+  private static IModel ConnectToRabbit()
+  {
     string? _rabbitmqName =
     Environment.GetEnvironmentVariable("rabbitmqContainerName");
-    var factory = new ConnectionFactory { HostName = _rabbitmqName, Port=5672 };
+    var factory = new ConnectionFactory { HostName = _rabbitmqName, Port = 5672 };
     var connection = factory.CreateConnection();
     IModel channel = connection.CreateModel();
     return channel;
   }
 
-  private static EventingBasicConsumer CreateRabbitConsumer() {
+  private static EventingBasicConsumer CreateRabbitConsumer()
+  {
     var consumer = new EventingBasicConsumer(rabbitChannel);
-    consumer.Received += (model, ea) => {
+    consumer.Received += (model, ea) =>
+    {
       var body = ea.Body.ToArray();
       string msg = Encoding.UTF8.GetString(body);
       HandleRabbitConsuming(msg);
@@ -48,7 +54,8 @@ public class Program {
     return consumer;
   }
 
-  private static void StartRabbitConsuming(EventingBasicConsumer consumer) {
+  private static void StartRabbitConsuming(EventingBasicConsumer consumer)
+  {
     string? queueName =
     Environment.GetEnvironmentVariable("rabbitLogsQueue");
     rabbitChannel.BasicConsume(
@@ -57,16 +64,20 @@ public class Program {
       consumer: consumer);
   }
 
-  private static void HandleRabbitConsuming(string msg) {
+  private static void HandleRabbitConsuming(string msg)
+  {
     _logTexts.Add(msg);
   }
 
-  private static IWebHost CreateWebHost() {
+  private static IWebHost CreateWebHost()
+  {
     var host = new WebHostBuilder()
       .UseKestrel()
       .UseUrls(_url)
-      .Configure(app => {
-        app.Run(async (context) => {
+      .Configure(app =>
+      {
+        app.Run(async (context) =>
+        {
           context.Response.ContentType = "text/plain";
           var text = string.Join("\n", _logTexts) + "\n";
           await context.Response.WriteAsync(text);
