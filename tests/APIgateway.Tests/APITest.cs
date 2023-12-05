29810c4220dd7d57a9d2ed37e4d01d4e8d138054
Joonas Pelttari <joonas.pelttari@gmail.com>
2023-12-05 18:38:57 +0200

    tests template for APIgateway

---
+++ tests/APIgateway.Tests/APITest.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class TestRESTAPI
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void GETmessages()
+    {
+        Assert.Fail("TESTS NOT IMPLEMENTED");
+    }
+    
+    [Test]
+    public void PUTstate()
+    {
+        Assert.Fail("TESTS NOT IMPLEMENTED");
+    }
+    
+    [Test]
+    public void GETstate()
+    {
+        Assert.Fail("TESTS NOT IMPLEMENTED");
+    }
+    
+    [Test]
+    public void GETrunlog()
+    {
+        Assert.Fail("TESTS NOT IMPLEMENTED");
+    }
+    
+    [Test]
+    public void GETmqstatistic()
+    {
+        Assert.Fail("TESTS NOT IMPLEMENTED");
+    }
+}
+
