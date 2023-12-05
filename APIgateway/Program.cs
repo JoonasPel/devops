9de4ade0c51c4d1851de0b881cbc83dccf867fac
Joonas Pelttari <joonas.pelttari@gmail.com>
2023-12-05 22:30:30 +0200

    Implement API: GET /messages

--- APIgateway/Program.cs
+++ APIgateway/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseKestrel(options => { options.ListenAnyIP(8083); });
+var app = builder.Build();
+HttpClient client = new HttpClient();
+
+app.MapGet("/messages", async () =>
+{
+  try
+  {
+    using HttpResponseMessage response = await client.GetAsync(
+      "http://monitorjoonaspelttari:8087");
+    response.EnsureSuccessStatusCode();
+    string body = await response.Content.ReadAsStringAsync();
+    return body;
+  }
+  catch
+  {
+    return "error getting logs from monitor\n";
+  }
+});
+
+app.Run();
