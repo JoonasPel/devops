fd66b4a079a756074e27ec46cde0c116377d06a5
Joonas Pelttari <joonas.pelttari@gmail.com>
2023-12-06 19:32:20 +0200

    Implement tests for API: GET+PUT /state

--- tests/APIgateway.Tests/APITest.cs
+++ tests/APIgateway.Tests/APITest.cs
@@ -41,15 +41,35 @@ public class TestRESTAPI
   }
 
   [Test]
-  public void PUTzstate()
+  public void PUTandGETstate()
   {
-    Assert.Ignore("TEST NOT IMPLEMENTED");
-  }
-
-  [Test]
-  public void GETstate()
-  {
-    Assert.Ignore("TEST NOT IMPLEMENTED");
+    // Change state to PAUSED and check it worked
+    RestRequest pauseRequest = new RestRequest("/state", Method.Put);
+    pauseRequest.AddParameter("text/plain", "PAUSED", ParameterType.RequestBody);
+    client.Execute(pauseRequest);
+    Thread.Sleep(1000);  // give the system time to change its state
+    RestRequest getStateRequest = new RestRequest("/state", Method.Get);
+    RestResponse response = client.Execute(getStateRequest);
+    Assert.AreEqual("PAUSED", response.Content);
+    // Change state to PAUSED again and check it is still PAUSED
+    client.Execute(pauseRequest);
+    Thread.Sleep(1000);
+    RestResponse response2 = client.Execute(getStateRequest);
+    Assert.AreEqual("PAUSED", response2.Content);
+    // Change state to RUNNING and check it worked
+    RestRequest runningRequest = new RestRequest("/state", Method.Put);
+    runningRequest.AddParameter("text/plain", "RUNNING", ParameterType.RequestBody);
+    client.Execute(runningRequest);
+    Thread.Sleep(1000);
+    RestResponse response3 = client.Execute(getStateRequest);
+    Assert.AreEqual("RUNNING", response3.Content);
+    // Change state to INIT and check it worked
+    RestRequest initRequest = new RestRequest("/state", Method.Put);
+    initRequest.AddParameter("text/plain", "INIT", ParameterType.RequestBody);
+    client.Execute(initRequest);
+    Thread.Sleep(1000);
+    RestResponse response4 = client.Execute(getStateRequest);
+    Assert.AreEqual("INIT", response4.Content);
   }
 
   [Test]
